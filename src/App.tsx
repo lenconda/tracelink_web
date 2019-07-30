cb1f15be5addf89610a40694ea597326514d96b2
lenconda <1053464288@qq.com>
2019-07-30 22:27:53 +0800

    :zap: Improve index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Button from './components/button/Button';
 import './App.scss';
 
 const App = (): JSX.Element => {
+  const [trackId, setTrackId] = useState<string>('');
+
   return (
     <>
       <div className="container-fluid h-100 d-flex justify-content-center align-items-center">
@@ -11,17 +13,36 @@ const App = (): JSX.Element => {
           <img className="index-logo" src="/assets/img/logo.svg" alt="site-logo" />
           <div className="index-input d-flex justify-content-center align-items-center row w-100">
             <div className="box-item-wrapper">
-              <input type="text" className="zi-input w-100 box-item" />
+              <input
+                type="text"
+                placeholder="Track ID. e.g. 44cb83fa3fff4ff79563aa7e2522b287"
+                className="zi-input w-100 box-item"
+                onChange={event => setTrackId(event.target.value)}
+              />
             </div>
           </div>
           <div className="index-button d-flex justify-content-center align-items-center row w-100">
             <div className="box-item-wrapper">
-              <Button className="w-100 box-item" type="primary" shadow={true}>Continue</Button>
+              <Button
+                className="w-100 box-item"
+                type="primary"
+                disabled={!trackId}
+                shadow={true}
+              >
+                Continue
+              </Button>
             </div>
           </div>
           <div className="index-button d-flex justify-content-center align-items-center row w-100">
             <div className="box-item-wrapper">
-              <Button className="w-100 box-item" type="success" shadow={true}>Create</Button>
+              <Button
+                className="w-100 box-item"
+                type="success"
+                onClick={() => { window.location.href = '/create' }}
+                shadow={true}
+              >
+                Create
+              </Button>
             </div>
           </div>
         </div>
