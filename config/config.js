0be37461a38b47936357e34a67551583cdfe6221
lenconda <1053464288@qq.com>
2019-08-04 23:49:27 +0800

    :bug: Fix npm script issues

--- config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'TraceLink',
-  isDev: process.env.NODE_ENV.toLowerCase() === 'development',
-  isProduction: process.env.NODE_ENV.toLowerCase() === 'production',
-  isTest: process.env.NODE_ENV.toLowerCase() === 'test'
-};
