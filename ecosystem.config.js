ecbb335bc3e694b41a2d2025e184fe1ab4118efa
Lenconda <i@lenconda.top>
2019-08-05 09:01:43 +0800

    :rocket: Add pm2 deployment configuration file

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [{
+    name: 'tracelink_web',
+    script: 'dist/server/index.js',
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy: {
+    production: {
+      user: 'web',
+      host: 'tracel.ink',
+      ref: 'origin/ssr',
+      repo: 'https://github.com/lenconda/tracelink_api.web',
+      path: '/home/web/space/tracelink_web',
+      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
